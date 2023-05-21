85c1a8c694e518b6ac5847131ba2e14bcb392c7d
maxamed1st <maxamed1st@icloud.com>
2023-05-21 18:05:21 +0100

    create and style footer

---
+++ components/footer.js
@@ -0,0 +1,10 @@
+
+export default function footer() {
+    return (
+        <footer className="bg-slate-900 text-white rounded-md p-2 mt-1">
+            <section className="mx-auto max-w-fit">
+                Made by <a href="https://www.linkedin.com/in/mohamed-hassan-ahmed-b629447a/" className="text-gray-400">Mohamed Hassan Ahmed</a>
+            </section>
+        </footer>
+    )
+}
