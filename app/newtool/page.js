b0a9f837e8afc56e72d0cef4e5adae80d5e6ebc1
maxamed1st <maxamed1st@icloud.com>
2023-05-28 22:45:51 +0100

    build interface to add new tools

--- app/newtool/page.js
+++ app/newtool/page.js
@@ -1,5 +1,24 @@
-export default function newTool() {
+'use client'
+import { useState } from "react"
+export default function newTool(handleSubmit) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
     return (
-        <h1>Hello Walaalka!</h1>
+        <main>
+            <form className="flex flex-col content-center items-center gap-1 [&>*]:w-2/3">
+                <label className="text-slate-900 text-xs -mb-1"> Name of the tool </label>
+                <input type="text" onChange={e => setTitle(e.target.value)}  value={title} required
+                className="focus:outline-lime-600 border-2 rounded border-lime-800"/>
+                <label className="text-slate-900 text-xs -mb-1"> Description </label>
+                <textarea rows="7" cols="10" onChange={e => setDescription(e.target.value)} value={description} required
+                className="focus:outline-lime-600 border-2 rounded border-lime-800 resize-none overflow-auto"/>
+                <label className="text-slate-900 text-xs -mb-1"> Category </label>
+                <input type="text-area" onChange={e => setCategory(e.target.value)} value={category} autoComplete="on"
+                className="focus:outline-lime-600 border-2 rounded border-lime-800"/>
+                <button type="submit" onClick={handleSubmit} 
+                className="bg-lime-600 hover:bg-lime-800 text-white font-bold py-2 px-4 rounded !w-[130px] mt-2"> Submit </button> 
+            </form>
+        </main>
     )
 }
