1bb9143516719eafed5d2189287ee16e4496c239
maxamed1st <maxamed1st@icloud.com>
2023-06-01 00:22:59 +0100

    add new tools to database

--- app/newtool/page.js
+++ app/newtool/page.js
@@ -1,14 +1,37 @@
 'use client'
 import { useState } from "react"
-export default function newTool(handleSubmit) {
+import { setDocument } from "@/utils/firebase";
+
+export default function newTool() {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [category, setCategory] = useState("");
+    const handleSubmit = async e => {
+        try {
+            e.preventDefault;
+            //Make sure the form is valid
+            if (title.trim().length === 0 || description.trim().length === 0) {
+                //Do nothing incase title or description is empty
+                return
+            }
+            //Add tool to database
+            await setDocument(
+                title, {
+                    "title" : title.trim(), "description" : description.trim(), "category" : category.trim()
+                    });
+            //set input fields to empty string
+            setTitle("");
+            setDescription("");
+            setCategory("");
+        } catch (e) {
+            console.error(e);
+        }
+    }
     return (
-        <main>
-            <form className="flex flex-col content-center items-center gap-1 [&>*]:w-2/3">
+        <main className="bg-white p-1">
+            <form onSubmit={e => e.preventDefault} className="flex flex-col content-center items-center gap-1 [&>*]:w-2/3">
                 <label className="text-slate-900 text-xs -mb-1"> Name of the tool </label>
-                <input type="text" onChange={e => setTitle(e.target.value)}  value={title} required
+                <input type="text" onChange={e => setTitle(e.target.value)}  value={title} autoFocus required
                 className="focus:outline-lime-600 border-2 rounded border-lime-800"/>
                 <label className="text-slate-900 text-xs -mb-1"> Description </label>
                 <textarea rows="7" cols="10" onChange={e => setDescription(e.target.value)} value={description} required
