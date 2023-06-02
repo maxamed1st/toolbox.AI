be914ca0e8a5639bcaf8ebc7a8638f8835d8d56a
maxamed1st <maxamed1st@icloud.com>
2023-06-02 23:24:07 +0100

    create 404 page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function pageNotFound() {
+    return(
+        <main className="bg-white grid place-items-center py-8">
+            <section>...ooops 🙄 <br/></section>
+            Seems the page you're looking for does not exist
+        </main>
+    )
+}
